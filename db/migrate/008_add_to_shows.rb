66e2e40fd4693a4e21edec5d3131c7eb20ea9c2e
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-17 01:55:37 +0000

    Automatically backed up by Learn

--- db/migrate/008_add_to_shows.rb
+++ db/migrate/008_add_to_shows.rb
@@ -1,6 +1,6 @@
 class AddToShows < ActiveRecord::Migration[4.2]
   def change 
-    add_column :shows, :genres, :string
+    add_column :shows, :genre, :string
     add_column :shows, :season, :string
     add_column :shows, :day, :string 
   end 
