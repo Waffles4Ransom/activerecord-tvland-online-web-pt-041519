8367b116a48a4cd10eb708fbf97f3dc9f52d1e88
Jenna Ransom <jenna.ransom@gmail.com>
2019-07-17 01:45:43 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,7 +2,8 @@ class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
   
+  #returns "#{urkel.name} always says: #{urkel.catchphrase}"
   def say_that_thing_you_say 
-    self.catchphrase
+    "#{self.name} always says: #{self.catchphrase}"
   end 
 end
